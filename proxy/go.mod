c2a4fa05b47565e55d5a5130d67df42ce13c633c
Muhammad Fadhlika <fadhlika@gmail.com>
2021-03-20 10:21:15 +0700

    initial commit

---
+++ proxy/go.mod
@@ -0,0 +1,11 @@
+module github.com/faldez/helloworld-tonic/proxy
+
+go 1.16
+
+require (
+	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b
+	github.com/golang/protobuf v1.5.1
+	github.com/grpc-ecosystem/grpc-gateway v1.16.0
+	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013 // indirect
+	google.golang.org/grpc v1.36.0
+)
---
+++ proxy/main.go
@@ -0,0 +1,47 @@
+// protoc -I . --grpc-gateway_out ./proxy/gen/go --grpc-gateway_opt logtostderr=true --grpc-gateway_opt paths=source_relative --proto_path=C:\Users\fadhlika\go/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis --go_out=plugins=grpc:./proxy/gen/go ./proto/helloworld.proto
+package main
+
+import (
+	"context"
+	"flag"
+	"net/http"
+
+	"github.com/golang/glog"
+	"github.com/grpc-ecosystem/grpc-gateway/runtime"
+	"google.golang.org/grpc"
+
+	gw "github.com/faldez/helloworld-tonic/proxy/gen/go/proto"
+)
+
+var (
+	// command-line options:
+	// gRPC server endpoint
+	grpcServerEndpoint = flag.String("grpc-server-endpoint", "localhost:9090", "gRPC server endpoint")
+)
+
+func run() error {
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	// Register gRPC server endpoint
+	// Note: Make sure the gRPC server is running properly and accessible
+	mux := runtime.NewServeMux()
+	opts := []grpc.DialOption{grpc.WithInsecure()}
+	err := gw.RegisterGreeterHandlerFromEndpoint(ctx, mux, *grpcServerEndpoint, opts)
+	if err != nil {
+		return err
+	}
+
+	// Start HTTP server (and proxy calls to gRPC server endpoint)
+	return http.ListenAndServe(":8081", mux)
+}
+
+func main() {
+	flag.Parse()
+	defer glog.Flush()
+
+	if err := run(); err != nil {
+		glog.Fatal(err)
+	}
+}
